aaab255b228496a0f1168b03207d9e807f3cd941
Rajan Agarwal <rajan.ag005@gmail.com>
2022-10-14 20:01:37 -0400

    initial

---
+++ pages/api/edges.js
@@ -0,0 +1,21 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+const json = {
+  nodes: [
+    { id: 1, label: "Node 1", color: "#e04141" },
+    { id: 2, label: "Node 2", color: "#e09c41" },
+    { id: 3, label: "Node 3", color: "#e0df41" },
+    { id: 4, label: "Node 4", color: "#7be041" },
+    { id: 5, label: "Node 5", color: "#41e0c9" }
+  ],
+  edges: [
+    { from: 1, to: 2 },
+    { from: 1, to: 3 },
+    { from: 2, to: 4 },
+    { from: 2, to: 5 }
+  ]
+}
+
+export default function handler(req, res) {
+  res.status(200).json({ nodes: json.nodes, edges: json.edges });
+}
